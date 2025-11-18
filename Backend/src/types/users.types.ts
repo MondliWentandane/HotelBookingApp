ff282f0217fbb25b1f3c05a8f3a3d76030dda880
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-11-18 10:06:22 +0200

    Update users.types.ts
    I added email to use for authentication

--- Backend/src/types/users.types.ts
+++ Backend/src/types/users.types.ts
@@ -4,8 +4,10 @@ export interface Users{
     user_id: number
     passsword_hash: string
     name:string
+    email:string
     phone_number:string
     role:user_role
     created_at:Date
     updated_at: Date
+
 }
